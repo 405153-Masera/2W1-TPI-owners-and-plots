9bd4b589366129a0f4814606f13fb009396c0432
406003_UlisesLara <406003@tecnicatura.frc.utn.edu.ar>
2024-10-15 15:07:57 -0300

    PostPlot
    Co-Authored-By: 114318-Ruiz, Facundo Nicolas <143039211+FacuRuizz1@users.noreply.github.com>
    Co-Authored-By: 405786MoroBenjamin <164823652+405786MoroBenjamin@users.noreply.github.com>
    Co-Authored-By: Gustavo Martin Masera <166140791+405153-Masera@users.noreply.github.com>
    Co-Authored-By: CamilaLpz <130025843+CamilaLpz@users.noreply.github.com>
    Co-Authored-By: Juan Manuel Sánchez <63590260+2w1-111111-SanchezJuanManuel@users.noreply.github.com>
    Co-Authored-By: pilarCifuentes <142746600+pilarCifuente@users.noreply.github.com>
    Co-Authored-By: Bertello-113991 <142693404+valentino113991@users.noreply.github.com>

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/get/GetPlotDto.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/get/GetPlotDto.java
@@ -1,4 +1,18 @@
 package ar.edu.utn.frc.tup.lc.iv.dtos.get;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetPlotDto {
+    private int id;
+    private int plot_number;
+    private int block_number;
+    private double total_area_in_m2;
+    private double built_area_in_m2;
+    private String plot_state;
+    private String plot_type;
 }
--- src/main/java/ar/edu/utn/frc/tup/lc/iv/services/implementations/PlotServiceImpl.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/services/implementations/PlotServiceImpl.java
@@ -4,16 +4,20 @@ import ar.edu.utn.frc.tup.lc.iv.dtos.get.GetPlotDto;
 import ar.edu.utn.frc.tup.lc.iv.dtos.get.GetPlotStateDto;
 import ar.edu.utn.frc.tup.lc.iv.dtos.get.GetPlotTypeDto;
 import ar.edu.utn.frc.tup.lc.iv.dtos.post.PostPlotDto;
+import ar.edu.utn.frc.tup.lc.iv.entities.PlotEntity;
 import ar.edu.utn.frc.tup.lc.iv.entities.PlotStateEntity;
 import ar.edu.utn.frc.tup.lc.iv.entities.PlotTypeEntity;
 import ar.edu.utn.frc.tup.lc.iv.repositories.PlotRepository;
 import ar.edu.utn.frc.tup.lc.iv.repositories.PlotStateRepository;
 import ar.edu.utn.frc.tup.lc.iv.repositories.PlotTypeRepository;
 import ar.edu.utn.frc.tup.lc.iv.services.interfaces.PlotService;
+import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,7 +38,21 @@ public class PlotServiceImpl implements PlotService {
 
     @Override
     public GetPlotDto createPlot(PostPlotDto postPlotDto) {
-        return null;
+        //Validamos si el el plot a crear existe (mismo numero de plot)
+        validatePlotNumber(postPlotDto.getPlot_number());
+        //Creamos la entity
+        PlotEntity plotEntity = new PlotEntity();
+        //Mapeamos con metodo
+        mapPlotPostToPlotEntity(plotEntity, postPlotDto);
+        //Guardamos el plot
+        PlotEntity savedPlot = plotRepository.save(plotEntity);
+        //Mapeamos el entity al GetPlotDto para poder mostrarlo
+        GetPlotDto getPlotDto = new GetPlotDto();
+        mapPlotEntityToGetPlotDto(savedPlot , getPlotDto );
+
+        //Retornamos el getPlotDto
+        return getPlotDto;
+
     }
 
     @Override
@@ -56,4 +74,47 @@ public class PlotServiceImpl implements PlotService {
         }
         return plotTypeDtos;
     }
+
+    //Metodo para validar si existe un plot con ese numero
+    public void validatePlotNumber(Integer plotNumber) {
+        if (plotRepository.findByPlotNumber(plotNumber) != null) {
+            throw new IllegalArgumentException("Error creating plot: Plot already exist.");
+        }
+    }
+
+    public void mapPlotPostToPlotEntity(PlotEntity plotEntity, PostPlotDto postPlotDto) {
+        //Seteamos campos basicos
+        plotEntity.setPlotNumber(postPlotDto.getPlot_number());
+        plotEntity.setBlockNumber(postPlotDto.getBlock_number());
+        plotEntity.setBuiltAreaInM2(postPlotDto.getBuilt_area_in_m2());
+        plotEntity.setTotalAreaInM2(postPlotDto.getTotal_area_in_m2());
+        plotEntity.setCreatedDatetime(LocalDateTime.now());
+        plotEntity.setLastUpdatedDatetime(LocalDateTime.now());
+        plotEntity.setCreatedUser(1);
+        plotEntity.setLastUpdatedUser(1);
+
+        //Mapeamos los estados y tipos
+        PlotStateEntity state = plotStateRepository.findById(postPlotDto.getPlot_state_id())
+                .orElseThrow(() -> new EntityNotFoundException("PlotState not found with id: " + postPlotDto.getPlot_state_id()));
+        PlotTypeEntity type = plotTypeRepository.findById(postPlotDto.getPlot_type_id())
+                .orElseThrow(() -> new EntityNotFoundException("PlotType not found with id: " + postPlotDto.getPlot_type_id()));
+
+        //Seteamos estados y tipos
+        plotEntity.setPlotType(type);
+        plotEntity.setPlotState(state);
+
+        //Guardamos en el repositorio
+        plotRepository.save(plotEntity);
+    }
+
+    public void mapPlotEntityToGetPlotDto(PlotEntity plotEntity, GetPlotDto getPlotDto) {
+        getPlotDto.setPlot_number(plotEntity.getPlotNumber());
+        getPlotDto.setBlock_number(plotEntity.getBlockNumber());
+        getPlotDto.setBuilt_area_in_m2(plotEntity.getBuiltAreaInM2());
+        getPlotDto.setTotal_area_in_m2(plotEntity.getTotalAreaInM2());
+        getPlotDto.setId(plotEntity.getId());
+        getPlotDto.setPlot_state(plotEntity.getPlotState().getName());
+        getPlotDto.setPlot_type(plotEntity.getPlotType().getName());
+    }
+
 }
