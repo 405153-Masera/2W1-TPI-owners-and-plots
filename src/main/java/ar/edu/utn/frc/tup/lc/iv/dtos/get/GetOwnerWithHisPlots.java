d7f04ad3851b2b4b5b1ad70e5f29c9fd31b9b5bc
113991-Bertello <113991@tecnicatura.frc.utn.edu.ar>
2024-11-03 21:34:01 -0300

    Owners con sus plotId

---
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/get/GetOwnerWithHisPlots.java
@@ -0,0 +1,30 @@
+package ar.edu.utn.frc.tup.lc.iv.dtos.get;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * DTO que representa una respuesta que contiene la información de
+ * un propietario, y su lote
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GetOwnerWithHisPlots {
+
+    /**
+     * Datos del propietario.
+     */
+    private OwnerDto owner;
+
+    /**
+     * Datos del lote del propietario.
+     */
+    private List<Integer> plot;
+
+}
