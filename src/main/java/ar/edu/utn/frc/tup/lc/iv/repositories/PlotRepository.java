95c873f84583600c4073647099e3bed44c867074
406003_UlisesLara <406003@tecnicatura.frc.utn.edu.ar>
2024-11-17 23:09:51 -0300

    dashboard

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/repositories/PlotRepository.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/repositories/PlotRepository.java
@@ -45,7 +45,7 @@ public interface PlotRepository extends JpaRepository<PlotEntity, Integer> {
      */
     @Query(value = "SELECT ps.name as stateName, COUNT(p.id) as count "
             + "FROM plots p "
-            + "JOIN plotstates ps ON ps.id = p.plot_state_id "
+            + "JOIN plot_states ps ON ps.id = p.plot_state_id "
             + "GROUP BY ps.name",
             nativeQuery = true)
     List<Object[]> countPlotsByState();
